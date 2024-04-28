3876cf486e7f6fb2ee457f803be2dc89ce32893f
Gokulasigamani <113229521+Gokulasigamani@users.noreply.github.com>
2024-04-28 21:31:23 +0530

    restart-first push

---
+++ app.js
@@ -0,0 +1,15 @@
+function down()
+{
+    let ld=document.getElementById('drop')
+
+    ld.classList.add('ld-down')
+    ld.classList.remove('d-none')
+}
+function down1()
+{
+    let ld=document.getElementById('drop')
+
+    ld.classList.remove('ld-down')
+    ld.classList.add('d-none')
+    
+}
